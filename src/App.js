1d0c0b5ab15bbf4021c9e65f5341bdf9b7a8c429
Lucas <lukasalves271@gmail.com>
2021-04-21 23:32:31 -0300

    add proptypes and more components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react'
+import Header from './Header'
+import Post from './Post'
+
+const posts = [
+  {title:'Title#01', subtitle:'Sub#01', likes: 20},
+  {title:'Title#02', subtitle:'Sub#02', likes: 10},
+  {title:'Title#03', subtitle:'Sub#03', likes: 50},
+]
 
 export default function App(){
   return (
-    <h1>Componente App</h1>
+    <>
+      <Header title="JStack's Blog">
+        <h2>Post da semana</h2>
+      </Header> 
+
+      <hr/>
+
+      {posts.map(post => (
+        <Post
+        key={post.title}
+        likes={post.likes} 
+        post={{
+          title:post.title,
+          subtitle:post.subtitle
+        }} 
+        />
+      ))}
+
+    </>
   )
 }
