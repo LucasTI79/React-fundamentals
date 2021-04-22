1e7d60191dc309687ac477e53439c22577f06d28
Lucas <lukasalves271@gmail.com>
2021-04-21 21:31:45 -0300

    add loaders in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpeckPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'transpiled', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle[fullhash].js',
@@ -13,5 +13,18 @@ module.exports = {
       template: path.resolve(__dirname,'public','index.html')
     }),
     new CleanWebpackPlugin(),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test:/\.css$/i, 
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude:/node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  }
 };
