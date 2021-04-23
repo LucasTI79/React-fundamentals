24001860052d6475fa83fd4eb1ff658d1598e882
Lucas <lukasalves271@gmail.com>
2021-04-23 00:30:40 -0300

    add state, callbacksProps and if's

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import Post from './Post'
 
-const posts = [
-  {title:'Title#01', subtitle:'Sub#01', likes: 20},
-  {title:'Title#02', subtitle:'Sub#02', likes: 10},
-  {title:'Title#03', subtitle:'Sub#03', likes: 50},
-]
-
 export default function App(){
+  const [posts, setPosts] = useState([ 
+    {id: Math.random(), title:'Title#01', subtitle:'Sub#01', likes: 20, read: false},
+    {id: Math.random(), title:'Title#02', subtitle:'Sub#02', likes: 10, read: true},
+    {id: Math.random(), title:'Title#03', subtitle:'Sub#03', likes: 50, read: false},
+  ])
+  function handleRefresh(){
+    setPosts(prevState => [...prevState, {
+        id: Math.random(),
+        title:`Title#0${prevState.length + 1}`,
+        subtitle:`Sub#${prevState.length + 1}`,
+        likes: 50
+    }])
+  }
+  function handleRemovePost(postId){
+    setPosts(prevState => (
+        prevState
+          .filter(post => post.id !== postId)
+      )
+    )
+  }
   return (
     <>
       <Header title="JStack's Blog">
-        <h2>Post da semana</h2>
+        <h2>
+          Post da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header> 
 
       <hr/>
 
       {posts.map(post => (
         <Post
-        key={post.title}
-        likes={post.likes} 
-        post={{
-          title:post.title,
-          subtitle:post.subtitle
-        }} 
+        key={post.id}
+        onRemove={handleRemovePost}
+        post={post} 
         />
       ))}
 
